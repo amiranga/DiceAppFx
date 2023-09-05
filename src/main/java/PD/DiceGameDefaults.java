e7ecbc5c1ca9aad549415d4a2b07de55b8b570bc
amiranga <ae.amunugama@gmail.com>
2023-09-05 22:33:38 +0400

    code formatted

--- src/main/java/PD/DiceGameDefaults.java
+++ src/main/java/PD/DiceGameDefaults.java
@@ -3,6 +3,6 @@ package PD;
 public interface DiceGameDefaults {
     public static final int NUMBER_OF_FACES = 6;
     public static final int NUMBER_OF_DICES = 1;
-    public static final String INVALID_INPUT_ERROR = "Invalid Input Parameter Detected";
+    public static final String INVALID_INPUT_ERROR = "Error: Parameters should be greater than 0";
     public static final String INPUT_VALUE = "";
 }
